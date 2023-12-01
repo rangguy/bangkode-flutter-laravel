5a4b366e0dae81f79effe065c9f60bb3bf91e145
rangguy <ranggadwi100@gmail.com>
2023-12-01 07:43:26 +0700

    Dashboard route kategoripage

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -158,7 +158,7 @@ class _DashboardState extends State<Dashboard> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => Nav(),
+                      builder: (context) => KategoriPage(),
                     ),
                   );
                 },
